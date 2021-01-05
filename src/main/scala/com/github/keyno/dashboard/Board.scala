231994b096b706b90928d715b45014a4f8a70b4c
Keyno <kofujiw@gmail.com>
2021-01-05 09:55:57 +0900

    switc route app sample

--- src/main/scala/com/github/keyno/dashboard/Board.scala
+++ src/main/scala/com/github/keyno/dashboard/Board.scala
@@ -1,10 +1,11 @@
 package com.github.keyno.dashboard
 
-import slinky.core.StatelessComponent
+import slinky.core.{FunctionalComponent, StatelessComponent, WithAttrs}
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
-import slinky.web.html._
-import slinky.web.svg.to
+import slinky.web.html.{div, hr, _}
+import typings.reactRouter.mod.{RouteProps, `match`}
+import typings.reactRouterDom.components.{BrowserRouter, Link, Route, Switch}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
@@ -23,6 +24,7 @@ object BoardCSS extends js.Object
       head(),
       body0(),
       body1(),
+      switch(),
     )
   }
 
@@ -80,6 +82,70 @@ object BoardCSS extends js.Object
   def createLinkList(linkUrl: String, text: String): ReactElement = {
     li()(
       a(href := linkUrl)(text)
+    )
+  }
+
+  // 空ページ
+  def home: WithAttrs[div.tag.type] = div(h2("Home"))
+
+  def switch(): ReactElement = {
+    div()(
+      BrowserRouter(
+        div(
+          ul(
+            li(Link[js.Object](to = "/")("Home")),
+            li(Link[js.Object](to = "/result")("Result")),
+          ),
+          hr(),
+          Switch(
+            Route(RouteProps().setExact(true).setPath("/").setRender(_ => home)),
+            Route(RouteProps().setPath("/result").setRender(props => ResultPage(props.`match`))),
+          )
+        )
+      )
+    )
+  }
+}
+
+@react object ResultPage {
+  type Props = `match`[_]
+
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { m =>
+    div(
+      h2("ResultPage"),
+      ul(
+        li(Link[js.Object](to = m.url + "/result")("Rendering with result"))
+      ),
+      hr(),
+      Switch(
+        Route(
+          RouteProps()
+            .setPath(m.path + "/:resultId")
+            .setRender(props => Result(props.`match`.asInstanceOf[`match`[Result.Param]]))
+        ),
+        Route(
+          RouteProps()
+            .setExact(true)
+            .setPath(m.path)
+            .setRender(_ => h3())
+        )
+      )
+    )
+  }
+}
+
+@react object Result {
+
+  @js.native
+  trait Param extends js.Object {
+    val resultId: String = js.native
+  }
+
+  case class Props(`match`: `match`[Result.Param])
+
+  val component: FunctionalComponent[Props] = FunctionalComponent[Props] { props =>
+    div(
+      h3("Result: " + props.`match`.params.resultId)
     )
   }
 }
